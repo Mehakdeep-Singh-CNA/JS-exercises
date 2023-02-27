60447a3a8905255768364edef4efbd3eaf3a1b17
mehakdeep-chhina <mehakdeep@cometrics.io>
2023-02-27 10:46:43 -0330

    Solution to loops problem

--- app with array and loops/index.js
+++ app with array and loops/index.js
@@ -1,12 +1,11 @@
 // declare and initialize an array to hold test scores from user
 const scores = [];
- 
+
 // use do-while loop to get scores from the user and store in array
 let score = 0;
 do {
     score = parseInt(prompt(
         "Enter a test score, or enter -1 to end scores", -1));
- 
     if (score >= 0 && score <= 100) {
         scores[scores.length] = score;
     }
@@ -23,10 +22,28 @@ if (scores.length > 0) {
         document.write(
             `<p>Score ${parseInt(i) + 1}: ${scores[i]}</p>`);
     }
- 
     //calculate and display the average
     const average = parseInt(total/scores.length);
     document.write(`<p>Average score is ${average}</p>`);
+    let newTotal = 0;
+    let newAverage = average;
+    do{
+        newTotal = 0;
+        for(let i in scores){
+            scores[i] = scores[i]/2;
+            newTotal = newTotal + scores[i];
+        }
+        newAverage = parseInt(newTotal/scores.length);
+    }
+    while(newAverage > 10);
+    for (let i in scores) {
+        document.write(
+            `<p>Score ${parseInt(i) + 1}: ${scores[i]}</p>`);
+    }
+    document.write(`<p>New Average score is ${newAverage}</p>`);
+
 }
+
+
 
 
